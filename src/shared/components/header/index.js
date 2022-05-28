7fef4bee549a71b7d6fbe995f23f67822da97763
nxNghia <nghia1421999@gmail.com>
2022-05-28 11:24:31 +0700

    account btn behavior

--- src/shared/components/header/index.js
+++ src/shared/components/header/index.js
@@ -1,11 +1,43 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './header.css';
 
 const Header = ({menuClickHandle}) => {
     const [showDropdown, setShowDropdown] = useState(false);
+    const clickOutside = useRef(null);
+    const clickAccountBtn = useRef(null);
+
+    useEffect(() => {
+        const clickOutsideHandle = e => {
+            if (clickAccountBtn.current && clickAccountBtn.current.contains(e.target)) {
+                if (showDropdown)
+                    setShowDropdown(false)
+            } else {
+                if (clickOutside.current && !clickOutside.current.contains(e.target)) {
+                    setShowDropdown(false);
+                }
+            }
+        }
+
+        document.addEventListener("mousedown", clickOutsideHandle);
+        return () => {
+            document.removeEventListener("mousedown", clickOutsideHandle);
+        }
+    }, [clickOutside, clickAccountBtn])
+
     const onMenuClickHandle = () => menuClickHandle();
 
-    const accountMenuClickHandle = () => setShowDropdown(showDropdown => !showDropdown);
+    const accountMenuClickHandle = () => setShowDropdown(showDropdown => {
+        if (showDropdown) {
+            return false;
+        }
+        return true;
+    });
+
+    const logOutClickHandle = () => {
+        //do something
+
+        setShowDropdown(false);
+    }
 
     return (
         <div className="header">
@@ -17,12 +49,12 @@ const Header = ({menuClickHandle}) => {
                     <i className="fa fa-bell" />
                 </div>
                 <div className='header__menu__account'>
-                    <p className='role-title' onClick={accountMenuClickHandle}>Quản lý</p>
+                    <p className={`role-title ${showDropdown && 'show--dropdown'}`} onClick={accountMenuClickHandle} ref={clickAccountBtn}>Quản lý</p>
                     <i className="fa fa-user" />
                     {
                         showDropdown &&
                         <div className='account__dropdown'>
-                            <div className='account__option'>
+                            <div className='account__option' ref={clickOutside} onClick={logOutClickHandle}>
                                 <p>Đăng xuất</p>
                                 <i className='fa fa-sign-out' />
                             </div>
