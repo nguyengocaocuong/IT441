bcfb5432aca2c8bd7fafabed718047deb5f77f66
Nguyen Ngo Cao Cuong <nguyenngocaocuong@gmail.com>
2022-06-12 05:32:51 +0000

    css

--- src/shared/components/content/main-content/index.js
+++ src/shared/components/content/main-content/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Chart from "react-apexcharts";
 
 import './main-content.css';
 import Filter from '../../filter';
@@ -10,6 +9,8 @@ import Badge from '../../badge';
 import Table from '../../table/index'
 import { Modal } from '@material-ui/core';
 import jsonData from '../../../../assets/jsonData/admin-main-content.json'
+import UserDetail from './user-detail/UserDetail';
+import RealEstateDetail from './real-estate-detail/RealEstateDetail';
 
 const renderHead = (item, index) => (
     <th key={index}>{item}</th>
@@ -21,68 +22,18 @@ const MainContent = ({ contentId = 0 }) => {
     const [dataUser, setDataUser] = useState(jsonData.dataValueUser)
     const [key, setKey] = useState(0)
     useEffect(() => {
-        if (contentId == 1) setData(jsonData.dataValue.filter(item => key == 0 || item.status == key))
-        if (contentId == 2) setDataUser(jsonData.dataValueUser.filter(item => key == 0 || item.status == key))
+        if (contentId === 1) setData(jsonData.dataValue.filter(item => key === 0 || item.status === key))
+        if (contentId === 2) setDataUser(jsonData.dataValueUser.filter(item => key === 0 || item.status === key))
     }, [key])
 
 
     const onCloseHandle = () => {
         setDetailInfo({ open: false })
     };
-    const getDetailUser = (user) => {
-        const max = 90;
-        const min = 30;
-        let newMixedSeries = []
-        jsonData.dataValueUser[0].chartOptions.seriesMixedChart.forEach((s) => {
-            const data = s.data.map(() => {
-                return Math.floor(Math.random() * (max - min + 1)) + min;
-            });
-            newMixedSeries.push({ data: data, type: s.type });
-        })
-        jsonData.dataValueUser[0].chartOptions.seriesMixedChart = newMixedSeries
-        return <div>
-            <div className='user__info'>
-                <div className='user__info-left'>
-                    <div className='info__header'>
-                        <h2>{user.name}</h2>
-                        <h4>Ngày đăng nhập cuối : <span>{user.lastLogin}</span></h4>
-                    </div>
-                    <div className='info__body'>
-                        <div className='info__body-left'>
-                            <p>Date of Birth : <span>{user.dob}</span></p>
-                            <p>Phone Number : <span>{user.phone}</span></p>
-                            <p>Email : <span>{user.email}</span></p>
-                            <p>Address: <span>{user.address}</span></p>
-                        </div>
-                        <div className='info__body-right'>
-                            <p>Rule: <span>{user.rule == 2 ? 'Người bán' : 'Người đăng'}</span></p>
-                            <p>Trạng thái: <span>{user.status == 1 ? 'Online' : 'Ofline'}</span></p>
-                        </div>
-                    </div>
-                </div>
-                <div className='user__info-right'>
-                    <div className='user__avatar'>
-                        <img src={user.avatar} />
-                    </div>
-                </div>
-            </div>
-            <div className='chart__kpi'>
-                <h3>Thống kê</h3>
-                <Chart
-                        options={jsonData.dataValueUser[0].chartOptions.optionsMixedChart}
-                        series={jsonData.dataValueUser[0].chartOptions.seriesMixedChart}
-                        type="line"
-                        width="100%"
-                        height="300px"
-                    />
-            </div>
-        </div>
-    }
-    const getDetailBDS = (bds) => {
+   
 
-    }
     const getDetailContent = () => (
-        detailInfo.open ? getDetailUser(detailInfo.item) : <></>
+        detailInfo.open ?( contentId === 2 ? <UserDetail user={detailInfo.item}/> : <RealEstateDetail realEstate={detailInfo.item}/>): <></>
     )
 
     const handleClick = (item) => {
@@ -95,7 +46,7 @@ const MainContent = ({ contentId = 0 }) => {
                 <td>{item.idNV}</td>
                 <td>{new Intl.NumberFormat().format(item.total)}</td>
                 <td>{item.date}</td>
-                <td><Badge content={item.status === 1 ? "Đã bán" : (item.status == 2 ? "Chưa bán" : "Bị Hủy")} type={item.status === 1 ? "success" : (item.status == 2 ? "primary" : "danger")} /></td>
+                <td><Badge content={item.status === 1 ? "Đã bán" : (item.status === 2 ? "Chưa bán" : "Bị Hủy")} type={item.status === 1 ? "success" : (item.status === 2 ? "primary" : "danger")} /></td>
             </tr>
         )
     }
@@ -106,7 +57,7 @@ const MainContent = ({ contentId = 0 }) => {
                 <td><i className="fa fa-user-circle sample"></i></td>
                 <td>{item.name}</td>
                 <td>{item.idNV}</td>
-                <td>{item == 2 ? 'Người bán' : 'Người đăng'}</td>
+                <td>{item === 2 ? 'Người bán' : 'Người đăng'}</td>
                 <td>{item.lastLogin}</td>
                 <td><Badge content={item.status === 1 ? "Online" : "Ofline"} type={item.status === 1 ? "success" : "danger"} /></td>
             </tr>
