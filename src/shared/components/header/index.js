e1c8637180d12e08aae5fe03f197a6c81ba27f98
nxNghia <nghia1421999@gmail.com>
2022-07-20 01:50:41 +0700

    add notification dropdown

--- src/shared/components/header/index.js
+++ src/shared/components/header/index.js
@@ -1,3 +1,4 @@
+import { Badge, Modal } from '@material-ui/core';
 import React from 'react';
 
 import { useState, useRef, useEffect } from 'react';
@@ -11,6 +12,8 @@ const roleTranslate = [
 
 const Header = ({menuClickHandle, userData, onSignOut}) => {
     const [showDropdown, setShowDropdown] = useState(false);
+    const [showDropdown2, setShowDropdown2] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const clickOutside = useRef(null);
     const clickAccountBtn = useRef(null);
 
@@ -18,10 +21,12 @@ const Header = ({menuClickHandle, userData, onSignOut}) => {
         const clickOutsideHandle = e => {
             if (clickAccountBtn.current && clickAccountBtn.current.contains(e.target)) {
                 if (showDropdown)
-                    setShowDropdown(false)
+                    setShowDropdown(false);
+                    setShowDropdown2(false);
             } else {
                 if (clickOutside.current && !clickOutside.current.contains(e.target)) {
                     setShowDropdown(false);
+                    setShowDropdown2(false);
                 }
             }
         }
@@ -53,7 +58,52 @@ const Header = ({menuClickHandle, userData, onSignOut}) => {
                 <div className='header__menu__control'>
                     <i className="fa fa-question-circle" />
                     <i className="fa fa-globe" />
-                    <i className="fa fa-bell" />
+                    <Badge badgeContent={3} color='error'>
+                        <i className="fa fa-bell" onClick={() => setShowDropdown2(true)} />
+                    </Badge>
+                    {
+                        showDropdown2 &&
+                        <div className='notification__dropdown'>
+                            <div className='account__option' ref={clickOutside} onClick={() => {}}>
+                                {
+                                    userData.role !== 0 && (
+                                        <>
+                                            <div className='notification__option'>
+                                                <div>Admin đã chỉ định bạn xử lí bất động sản <span className='link' onClick={() => {
+                                                    setShowModal(true);
+                                                    setShowDropdown2(false);
+                                                }}>ABC-001</span></div>
+                                                <div className='button-group'>
+                                                    <button className='accept-btn'>Chấp nhận</button>
+                                                    <button className='deny-btn'>Không chấp nhận</button>
+                                                </div>
+                                            </div>
+                                            <div className='notification__option'>
+                                                <div>Admin đã chỉ định bạn xử lí bất động sản <span className='link' onClick={() => {
+                                                    setShowModal(true);
+                                                    setShowDropdown2(false);
+                                                }}>ABC-001</span></div>
+                                                <div className='button-group'>
+                                                    <button className='accept-btn'>Chấp nhận</button>
+                                                    <button className='deny-btn'>Không chấp nhận</button>
+                                                </div>
+                                            </div>
+                                            <div className='notification__option'>
+                                                <div>Admin đã chỉ định bạn xử lí bất động sản <span className='link' onClick={() => {
+                                                    setShowModal(true);
+                                                    setShowDropdown2(false);
+                                                }}>ABC-001</span></div>
+                                                <div className='button-group'>
+                                                    <button className='accept-btn'>Chấp nhận</button>
+                                                    <button className='deny-btn'>Không chấp nhận</button>
+                                                </div>
+                                            </div>
+                                        </>
+                                    )
+                                }
+                            </div>
+                        </div>
+                    }
                 </div>
                 <div className='header__menu__account'>
                     <p className={`role-title ${showDropdown && 'show--dropdown'}`} onClick={accountMenuClickHandle} ref={clickAccountBtn}>{roleTranslate[userData.role].title}</p>
@@ -69,6 +119,41 @@ const Header = ({menuClickHandle, userData, onSignOut}) => {
                     }
                 </div>
             </div>
+            <Modal
+                open={showModal && userData.role !== 0}
+                onClose={() => setShowModal(false)}
+            >
+                <div className='detail-modal'>
+                    <div className='category'>
+                        <span>Địa chỉ:</span>
+                        <span> số 1 Trần Đại Nghĩa</span>
+                    </div>
+                    <div className='category'>
+                        <span>Diện tích:</span>
+                        <span> 100m2</span>
+                    </div>
+                    <div className='category'>
+                        <span>Số phòng:</span>
+                        <span> 2PN - 1WC</span>
+                    </div>
+                    <div className='category'>
+                        <span>Hướng:</span>
+                        <span>Đông Nam</span>
+                    </div>
+                    <div className='category'>
+                        <span>Giá bán:</span>
+                        <span> 2.5 tỷ</span>
+                    </div>
+                    <div className='category'>
+                        <span>Thông tin thêm:</span>
+                        <span> Nhà có sổ đỏ, nội thất đầy đủ</span>
+                    </div>
+                    <div className='button-group'>
+                        <button className='accept-btn' onClick={() => setShowModal(false)}>Chấp nhận</button>
+                        <button className='deny-btn' onClick={() => setShowModal(false)}>Không chấp nhận</button>
+                    </div>
+                </div>
+            </Modal>
         </div>
     )
 }
